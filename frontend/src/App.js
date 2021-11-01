9e7186a6e30aaba3260711f52351e1de103cba9f
HeyItsGwen <52086594+HeyItsGwen@users.noreply.github.com>
2021-10-31 22:14:32 -0500

    started work on dummy login logic (NOT WORKING)
    Committing this to a branch so someone can take a look and see if they can figure out what's going on and why i'm getting an "invalid hook call" error just trying to use MUI as the docs tell me to

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import LoginButton from './components/LoginButton';
 
 function App() {
   return (
     <div className="App">
       <h1>NorthArk Financial</h1>
+      <LoginButton />
     </div>
   );
 }
---
+++ frontend/src/components/LoginButton.js
@@ -0,0 +1,12 @@
+import Button from '@mui/material/Button';
+
+const LoginButton = () => {
+    
+    return (
+        <div>
+            <Button variant="contained">Hello World</Button>
+        </div>
+    )
+}
+
+export default LoginButton;
