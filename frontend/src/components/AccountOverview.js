e31a1ff31d0cea6291cab213cc8cc65651371efb
HeyItsGwen <52086594+HeyItsGwen@users.noreply.github.com>
2021-11-10 16:42:52 -0600

    Add navigation dropdown for authorized user navigation
    Added a MUI dropdown list to facilitate navigation between pages for currently logged in users. Currently leads between account summary page and reports and vice versa, and has a placeholder link to be replaced with an account upgrade page.

---
+++ frontend/src/components/AccountOverview.js
@@ -0,0 +1,10 @@
+
+export const AccountOverview = () => {
+
+    return (
+        <div className='overviewDiv'>
+            <h2 sx={{fontSize:'5px'}}>Account Overview</h2>
+            <p>placeholder for the actual account details returned from plaid</p>
+        </div>
+    )
+}
