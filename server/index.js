b959d0f340558f0e897877b5d71f0c653d7345ab
SilasCundiff <silinski23@gmail.com>
2021-11-15 15:37:10 -0500

    add small test route to test client-server connection

--- server/index.js
+++ server/index.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const plaid = require('plaid');
+const cors = require('cors');
+
 const app = express();
-const port = 3000;
+const port = 8080;
+
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
+});
+
+app.get('/api/test', (req, res) => {
+  res.send({ id: 1, first: 'John', last: 'Doe', balance: 100 });
 });
 
 app.listen(port, () => {
